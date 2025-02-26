160cde8ac72292d211e6b058897407b846eae55d
Iyamurinze <iymzjeremie307@example.com>
2025-02-26 16:09:16 +0200

    vercel.json

--- Backend/db/dbs.js
+++ Backend/db/dbs.js
@@ -1,27 +1,20 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,  // Add port explicitly
-        dialect: process.env.DB_DIALECT,
-        logging: false,  // Disable logging (optional)
-        pool: {
-            max: 10,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        },
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres", // Specify dialect explicitly
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false, // Disable logging (optional)
+});
 
 // Test the connection
 sequelize.authenticate()
-    .then(() => console.log('PostgreSQL database connected successfully'))
-    .catch(err => console.error('Unable to connect to the PostgreSQL database:', err));
+  .then(() => console.log("PostgreSQL database connected successfully"))
+  .catch((err) => console.error("Unable to connect to the PostgreSQL database:", err));
 
 module.exports = sequelize;
